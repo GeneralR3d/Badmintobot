650b365274298bfde712c077988b4b7400baed0e
TdrDingren <97043423+TdrDingren@users.noreply.github.com>
2023-09-14 15:34:07 +0800

    edits

--- predictcode.py
+++ predictcode.py
@@ -1,27 +1,28 @@
 import pandas as pd
 import numpy as np
 import pickle
+import math
 
 # Load the trained model
 with open('trained_model.pkl', 'rb') as file:
     gbm = pickle.load(file)
 
-# time_interval = 0.01
-# end_time = 2
+time_interval = 0.01
+end_time = 2
 
-#time_points = np.arange(0, end_time + time_interval, time_interval)
-time=[]
-i=0
-flag=2
-endTime=1.9
-while i < endTime:
-    if flag%3==0:
-        i+=0.04
-    else:
-        i+=0.03
-    flag+=1
-    time.append(i)
-time_points=np.array(time)
+time_points = np.arange(0, end_time + time_interval, time_interval)
+# time=[]
+# i=0
+# flag=2
+# endTime=1.9
+# while i < endTime:
+#     if flag%3==0:
+#         i+=0.04
+#     else:
+#         i+=0.03
+#     flag+=1
+#     time.append(i)
+# time_points=np.array(time)
 
 #Iznput
 # input_data = pd.DataFrame({
@@ -36,14 +37,32 @@ time_points=np.array(time)
 
 visionTraj=pd.read_excel("Vision Trajectory2.xlsx")
 
+#In vision trajectory 
+# X is wrt width of court
+# Y is wrt length of court
+# Z is wrt height of court
+LaunchX= visionTraj["x"][0]
+LaunchY= visionTraj["y"][0]
+LaunchZ= visionTraj["z"][0]
+changeX= visionTraj["x"][1]-visionTraj["x"][0]
+changeY= visionTraj["y"][1]-visionTraj["y"][0]
+changeZ= visionTraj["z"][1]-visionTraj["z"][0]
+
 input_data = pd.DataFrame({
     "time": time_points,
-    "LaunchX": visionTraj["x"],
-    "LaunchY": visionTraj["y"],
-    "LaunchZ": visionTraj["z"],
-    "LaunchAngle": 40,
-    "LaunchDirection": 15,
-    "InitialV": 100
+    "LaunchX": LaunchX, #note these can be swapped cos in the trained model X is wrt to court
+    "LaunchY": LaunchY,
+    "LaunchZ": LaunchZ,
+    "LaunchAngle": math.atan((changeZ)/(math.sqrt(changeX**2 + changeY**2))) * (180/math.pi),
+      #calculate from arctan(opp/adj) which is arctan(changez/change in xandy) which is arctan(changez/sqrt(changex^2 + changey^2))
+      #essentially calculated from a rotation around the y-axis
+    "LaunchDirection": math.atan((changeX)/(math.sqrt(changeY**2 + changeZ**2))) * (180/math.pi),
+        #calculate from arctan(opp/adj) which is arctan(change in x/change in yandz) which is arctan( changex/sqrt(changey^2 + changez^2)
+        #calculated from a rotation around the z-axis
+    "InitialV": 
+        #calcuate from change in the 3d position over change in time period
+        #recall velocity is a vector so the initialV can only be length of that vector
+        #vector is a 
 })
 
 
--- trainingcode.py
+++ trainingcode.py
@@ -5,8 +5,16 @@ from sklearn.ensemble import GradientBoostingRegressor
 import pickle
 
 data = pd.read_excel("dataset3.xlsx")
+
+#swap the X and Y so that
+# X is refering to lengthwise
+# Y is referring to widthwise
+newIndex= ["time",	"LocationY","LocationX","LocationZ","LaunchY",	"LaunchX","LaunchZ","LaunchAngle",	"LaunchDirection",	"InitialV",	"label"]
+data=data.reindex(columns=newIndex)
+
+
 X = data[["time", "LaunchX", "LaunchY", "LaunchZ", "LaunchAngle", "LaunchDirection", "InitialV"]]
-y = data[["LocationX","LocationY","LocationZ"]]
+y = data["LocationX"]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 gbm = GradientBoostingRegressor(n_estimators=300, learning_rate=0.1, random_state=42, max_depth=50, max_features='sqrt')
 gbm.fit(X_train, y_train)
