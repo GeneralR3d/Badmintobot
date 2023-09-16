d6f5f10da2697b95e92475447943cecf754fc34e
TdrDingren <97043423+TdrDingren@users.noreply.github.com>
2023-09-16 13:11:36 +0800

    made it easier to train models and predict code

--- predictcode.py
+++ predictcode.py
@@ -3,37 +3,10 @@ import numpy as np
 import pickle
 import math
 
-# Load the trained model
-with open('trained_modelY.pkl', 'rb') as file:
-    gbm = pickle.load(file)
-
 time_interval = 1/30
 end_time = 2
 
 time_points = np.arange(0, end_time + time_interval, time_interval)
-# time=[]
-# i=0
-# flag=2
-# endTime=1.9
-# while i < endTime:
-#     if flag%3==0:
-#         i+=0.04
-#     else:
-#         i+=0.03
-#     flag+=1
-#     time.append(i)
-# time_points=np.array(time)
-
-#Iznput
-# input_data = pd.DataFrame({
-#     "time": time_points,
-#     "LaunchX": 200,
-#     "LaunchY": 50,
-#     "LaunchZ": 1800,
-#     "LaunchAngle": 40,
-#     "LaunchDirection": 15,
-#     "InitialV": 100
-# })
 
 visionTraj=pd.read_excel("Vision Trajectory2.xlsx")
 
@@ -48,35 +21,81 @@ changeX= (visionTraj["x"][1]-visionTraj["x"][0]) *10
 changeY= (visionTraj["y"][1]-visionTraj["y"][0]) *10
 changeZ= (visionTraj["z"][1]-visionTraj["z"][0]) *10
 
-input_data = pd.DataFrame({
-    "time": time_points,
-    "LaunchX": LaunchY, #note these can be swapped cos in the trained model X is wrt to court
-    "LaunchY": LaunchX,
-    "LaunchZ": LaunchZ,
-    "LaunchAngle": math.atan((changeZ)/(math.sqrt(changeX**2 + changeY**2))) * (180/math.pi),
-      #calculate from arctan(opp/adj) which is arctan(changez/change in xandy) which is arctan(changez/sqrt(changex^2 + changey^2))
-      #essentially calculated from a rotation around the y-axis
-    "LaunchDirection": math.atan((changeY)/(math.sqrt(changeX**2 + changeZ**2))) * (180/math.pi),
-        #calculate from arctan(opp/adj) which is arctan(change in x/change in yandz) which is arctan( changex/sqrt(changey^2 + changez^2)
-        #calculated from a rotation around the z-axis
-    "InitialV": (math.sqrt(changeX**2 + changeY**2 + changeZ**2)/0.03)/1000
-        #calcuate from change in the 3d position over change in time period
-        #recall velocity is a vector so the initialV can only be length of that vector
-        #vector is a difference in all 3 coordinates
-})
+def main():
+    # Load the trained model
+    with open('trained_modelX.pkl', 'rb') as file:
+        gbm = pickle.load(file)
+        pred_gbm=predictData(gbm)
+
+        # Output the predicted data
+        predicted_data = pd.DataFrame({
+            "time": time_points,
+            "LocationX": pred_gbm
+        })
+        predicted_data.to_excel("predicted_trajectoriesX.xlsx", index=False)
+
+    # Load the trained model
+    with open('trained_modelY.pkl', 'rb') as file:
+        gbm = pickle.load(file)
+        pred_gbm=predictData(gbm)
+
+        # Output the predicted data
+        predicted_data = pd.DataFrame({
+            "time": time_points,
+            "LocationY": pred_gbm
+        })
+        predicted_data.to_excel("predicted_trajectoriesY.xlsx", index=False)
+
+    # Load the trained model
+    with open('trained_modelZ.pkl', 'rb') as file:
+        gbm = pickle.load(file)
+        pred_gbm=predictData(gbm)
+
+        # Output the predicted data
+        predicted_data = pd.DataFrame({
+            "time": time_points,
+            "LocationZ": pred_gbm
+        })
+        predicted_data.to_excel("predicted_trajectoriesZ.xlsx", index=False)
 
 
-y_pred_gbm = gbm.predict(input_data)
 
-# Output the predicted data
-predicted_data = pd.DataFrame({
-    "time": input_data["time"],
-    "LocationY": y_pred_gbm
-})
+#Iznput
+# input_data = pd.DataFrame({
+#     "time": time_points,
+#     "LaunchX": 200,
+#     "LaunchY": 50,
+#     "LaunchZ": 1800,
+#     "LaunchAngle": 40,
+#     "LaunchDirection": 15,
+#     "InitialV": 100
+# })
+def predictData(gbm):
+    input_data = pd.DataFrame({
+        "time": time_points,    
+        "LaunchX": LaunchY, #note these can be swapped cos in the trained model X is wrt to court
+        "LaunchY": LaunchX,
+        "LaunchZ": LaunchZ,
+        "LaunchAngle": math.atan((changeZ)/(math.sqrt(changeX**2 + changeY**2))) * (180/math.pi),
+        #calculate from arctan(opp/adj) which is arctan(changez/change in xandy) which is arctan(changez/sqrt(changex^2 + changey^2))
+        #essentially calculated from a rotation around the y-axis
+        "LaunchDirection": math.atan((changeY)/(math.sqrt(changeX**2 + changeZ**2))) * (180/math.pi),
+            #calculate from arctan(opp/adj) which is arctan(change in x/change in yandz) which is arctan( changex/sqrt(changey^2 + changez^2)
+            #calculated from a rotation around the z-axis
+        "InitialV": (math.sqrt(changeX**2 + changeY**2 + changeZ**2)/0.03)/1000
+            #calcuate from change in the 3d position over change in time period
+            #recall velocity is a vector so the initialV can only be length of that vector
+            #vector is a difference in all 3 coordinates
+    })
+    y_pred_gbm = gbm.predict(input_data)
+    
+    return y_pred_gbm
 
-predicted_data.to_excel("predicted_trajectoriesY.xlsx", index=False)
+
 #existing_data = pd.read_excel("predicted_trajectories.xlsx")
 
 #existing_data['LocationY'] = y_pred_gbm
 
 #existing_data.to_excel("predicted_trajectories.xlsx", index=False)
+
+main()
--- trainingcode.py
+++ trainingcode.py
@@ -4,7 +4,20 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import GradientBoostingRegressor
 import pickle
 
+
 data = pd.read_excel("dataset3.xlsx")
+
+def main():
+    with open('trained_modelX.pkl', 'wb') as file:
+        gbm=train("LocationX")
+        pickle.dump(gbm, file)
+    with open('trained_modelY.pkl', 'wb') as file:
+        gbm=train("LocationY")
+        pickle.dump(gbm, file)
+    with open('trained_modelY.pkl', 'wb') as file:
+        gbm=train("LocationZ")
+        pickle.dump(gbm, file)
+    
 
 #swap the X and Y so that
 # X is refering to lengthwise
@@ -14,12 +27,12 @@ data = pd.read_excel("dataset3.xlsx")
 #newIndex= ["time",	"LocationY","LocationX","LocationZ","LaunchY",	"LaunchX","LaunchZ","LaunchAngle",	"LaunchDirection",	"InitialV",	"label"]
 #data=data.reindex(columns=newIndex)
 
+def train(output_col):
+    X = data[["time", "LaunchX", "LaunchY", "LaunchZ", "LaunchAngle", "LaunchDirection", "InitialV"]]
+    y = data[output_col]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    gbm = GradientBoostingRegressor(n_estimators=300, learning_rate=0.1, random_state=42, max_depth=50, max_features='sqrt')
+    gbm.fit(X_train, y_train)
+    return gbm
 
-X = data[["time", "LaunchX", "LaunchY", "LaunchZ", "LaunchAngle", "LaunchDirection", "InitialV"]]
-y = data["LocationZ"]
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-gbm = GradientBoostingRegressor(n_estimators=300, learning_rate=0.1, random_state=42, max_depth=50, max_features='sqrt')
-gbm.fit(X_train, y_train)
 
-with open('trained_modelZ.pkl', 'wb') as file:
-    pickle.dump(gbm, file)
